4b0ea3430390c3f882e8096045b7e95f6583bb40
Elias Tornay Paz <etornay-@c2r7s5.42malaga.com>
2024-01-11 16:57:49 +0100

    paquito

--- src/minishell.c
+++ src/minishell.c
@@ -6,8 +6,35 @@
 /*   By: etornay- <etornay-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:10:25 by etornay-          #+#    #+#             */
-/*   Updated: 2024/01/10 16:34:22 by etornay-         ###   ########.fr       */
+/*   Updated: 2024/01/11 16:47:08 by etornay-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	minishell(t_paco *p, char **env)
+{
+	ft_paco_sanz();
+	while (33)
+	{
+		if (prompt(p) == EXIT_FAILURE)
+			exit (EXIT_FAILURE);
+		if (ft_strncmp(p->line, "\0", 1) == EXIT_FAILURE)
+			add_history(p->line);
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_paco	*p;
+
+	(void)argc;
+	(void)argv;
+	p = ft_calloc(1, sizeof(t_paco));
+	if (p == NULL)
+		exit (EXIT_FAILURE);
+	/* init_struct(p); */
+	if (minishell(p, env) == EXIT_FAILURE)
+		exit (EXIT_FAILURE);
+	exit (EXIT_SUCCESS);
+}
--- src/minishell.h
+++ src/minishell.h
@@ -6,13 +6,26 @@
 /*   By: etornay- <etornay-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:34:31 by etornay-          #+#    #+#             */
-/*   Updated: 2024/01/10 16:37:31 by etornay-         ###   ########.fr       */
+/*   Updated: 2024/01/11 16:36:57 by etornay-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../Libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# define RED_T "\x1b[1;31m"
+# define YELLOW_T "\x1b[1;33m"
+# define PURPLE_T "\033[35m"
+# define RESET_COLOR "\x1b[0m"
 
+typedef struct s_paco
+{
+	char	*shell;
+	char	*line;
+}	t_paco;
 
 #endif
