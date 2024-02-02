666cace8458e1c08ccd1502c485126128b18ca57
Nestor Cruz Gambero <ncruz-ga@c2r4s3.42malaga.com>
2024-02-02 12:02:05 +0100

    planteo echo

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 08:51:03 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/01/30 13:30:02 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/02/02 12:01:49 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,39 @@ void	pwd(t_paco *p)
 	ft_printf("%s\n", p->act_dir);
 }
 
-/* void	echo(t_paco *p, int flag)
+void	exec_echo(char **s, int size)
 {
-	
-} */
+	int	i;
+
+	i = 0;
+	while (s[size][i])
+	{
+		
+	}
+}
+
+void	echo(char **s, int flag)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (i > 1)
+	{
+		if (ft_strncmp (s[2], "-n\0", 3) == EXIT_FAILURE)
+		{
+			flag = 1;
+			i = 3;
+		}
+		else
+			i = 2;
+		while (s[i] != NULL)
+		{
+			exec_echo(s, i);
+			i++;
+		}
+	}
+	if (flag == 0)
+		printf("\n");
+}
